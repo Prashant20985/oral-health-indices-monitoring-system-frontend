bf640ce3736efefc1bc7feeaad9ca09aa635d3f0
Prashant20985 <prashantsharma4931@gmail.com>
2023-11-24 15:09:35 +0100

    Added No Rows Found Component

---
+++ src/app/common/NoRowsFound/NoRowsFound.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from "@mui/material";
+
+export default function NoRowsFound() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60vh",
+        flexDirection: "column",
+      }}
+    >
+      <img
+        src="/assets/noRowsFound.webp"
+        alt="No Rows Found"
+        loading="lazy"
+        height={200}
+        width={200}
+      />
+      <Typography variant="h6" fontWeight={600} textTransform="uppercase">
+        No Rows Found
+      </Typography>
+    </Box>
+  );
+}
