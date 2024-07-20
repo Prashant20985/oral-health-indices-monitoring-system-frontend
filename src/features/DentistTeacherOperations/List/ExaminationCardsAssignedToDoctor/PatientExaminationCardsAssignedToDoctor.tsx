47a663c264b944cdd5977b606f19384a664b9f53
harsh-S21901 <s21901@pjwstk.edu.pl>
2024-07-21 00:09:37 +0200

    Fixed build error

---
+++ src/features/DentistTeacherOperations/List/ExaminationCardsAssignedToDoctor/PatientExaminationCardsAssignedToDoctor.tsx
@@ -0,0 +1,123 @@
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../../../app/stores/Store";
+import React from "react";
+import PatientExaminationCardsList from "../../../ExaminationCardOperations/List/PatientExaminationCardsList";
+import { Autocomplete, Box, IconButton, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import Header from "../../../../app/common/header/Header";
+import { Clear } from "@mui/icons-material";
+
+export default observer(function PatientExaminationCardsAssignedToDoctor() {
+  const {
+    patientExaminationCardStore: {
+      getPatientExaminationCardsAssignedToDoctor,
+      groupedPatientExaminationCardsAssignedToDoctor,
+      loading,
+      setStudentId,
+      setMonth,
+      setYear,
+      year,
+      month,
+    },
+    dentistTeacherStore: {
+      fetchSupervisedStudents,
+      supervisedStudentNameEmailWithId,
+    },
+  } = useStore();
+
+  React.useEffect(() => {
+    const fetchCards = async () => {
+      await getPatientExaminationCardsAssignedToDoctor();
+    };
+    fetchCards();
+  }, [getPatientExaminationCardsAssignedToDoctor]);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mb={4}
+        mt={2}
+      >
+        <Box>
+          <Header
+            title="Examination Cards"
+            subTitle="Examination Cards assigned to Doctor"
+          />
+        </Box>
+        <Box display="flex" gap={4}>
+          <Box>
+            <Autocomplete
+              options={supervisedStudentNameEmailWithId}
+              loading={supervisedStudentNameEmailWithId.length === 0}
+              onFocus={() => {
+                if (supervisedStudentNameEmailWithId.length === 0) {
+                  fetchSupervisedStudents();
+                }
+              }}
+              getOptionLabel={(option) => option.name}
+              onChange={(_event, value) => {
+                if (value) {
+                  setStudentId(value.id);
+                } else {
+                  setStudentId("");
+                }
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  fullWidth
+                  label="Select Student"
+                  variant="outlined"
+                  color="secondary"
+                  sx={{
+                    width: 300,
+                  }}
+                />
+              )}
+            />
+          </Box>
+          <Box display="flex" gap={1} alignItems="center">
+            <DatePicker
+              slotProps={{
+                textField: {
+                  variant: "outlined",
+                  color: "secondary",
+                },
+              }}
+              value={new Date(year, month - 1)}
+              label="Select Year and Month"
+              views={["month", "year"]}
+              onChange={(date: Date | null) => {
+                console.log(date);
+                if (date) {
+                  setMonth(date.getMonth() + 1);
+                  setYear(date.getFullYear());
+                }
+              }}
+            />
+            <IconButton
+              onClick={() => {
+                setMonth(new Date().getMonth() + 1);
+                setYear(new Date().getFullYear());
+              }}
+              disabled={
+                month === new Date().getMonth() + 1 &&
+                year === new Date().getFullYear()
+              }
+            >
+              <Clear />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+      <PatientExaminationCardsList
+        patientExaminationCards={groupedPatientExaminationCardsAssignedToDoctor}
+        loading={loading.getPatientExaminationCardsAssignedToDoctor}
+      />
+    </LocalizationProvider>
+  );
+});
