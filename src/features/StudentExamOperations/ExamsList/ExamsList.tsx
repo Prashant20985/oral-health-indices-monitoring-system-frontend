4c8b0ca516c47294ce09ce578198bd3790e44799
Zeynep Sila Saglam <74879376+zeynepsilasaglam@users.noreply.github.com>
2024-08-15 13:09:13 +0200

    Update ExamsList.tsx

--- src/features/StudentExamOperations/ExamsList/ExamsList.tsx
+++ src/features/StudentExamOperations/ExamsList/ExamsList.tsx
@@ -4,15 +4,16 @@ import { useStore } from "../../../app/stores/Store";
 import { useParams } from "react-router-dom";
 import { Alert, Box, Grid, Typography } from "@mui/material";
 import StudentExamCard from "./StudentExamCard";
+import LoadingComponent from "../../../app/common/loadingComponents/LoadingComponent";
 
 /**
  * Renders a list of exams for a student.
- * 
+ *
  * @returns The JSX element representing the exams list.
  */
 export default observer(function ExamsList() {
   const {
-    studentExamStore: { groupedStudentExams, fetchStudentExams },
+    studentExamStore: { groupedStudentExams, fetchStudentExams, loading },
   } = useStore();
 
   const { id } = useParams<string>();
@@ -25,29 +26,35 @@ export default observer(function ExamsList() {
 
   return (
     <Box>
-      {groupedStudentExams.length > 0 ? (
-        <>
-          {groupedStudentExams.map(([group, exams]) => (
-            <Box mb={1}>
-              <Typography variant="h5" fontWeight="bold">
-                {group}
-              </Typography>
-              <>
-                <Grid container spacing={1} sx={{ p: 0.5, width: "100%" }}>
-                  {exams.map((exam) => (
-                    <Grid item md={6} lg={3} sm={12} xs={12} key={exam.id}>
-                      <StudentExamCard exam={exam} />
-                    </Grid>
-                  ))}
-                </Grid>
-              </>
-            </Box>
-          ))}
-        </>
+      {loading.studentExams ? (
+        <LoadingComponent />
       ) : (
-        <Alert severity="info" variant="outlined" sx={{ mt: 2 }}>
-          No Exams Published
-        </Alert>
+        <>
+          {groupedStudentExams.length > 0 ? (
+            <>
+              {groupedStudentExams.map(([group, exams]) => (
+                <Box mb={1}>
+                  <Typography variant="h5" fontWeight="bold">
+                    {group}
+                  </Typography>
+                  <>
+                    <Grid container spacing={1} sx={{ p: 0.5, width: "100%" }}>
+                      {exams.map((exam) => (
+                        <Grid item md={6} lg={3} sm={12} xs={12} key={exam.id}>
+                          <StudentExamCard exam={exam} />
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </>
+                </Box>
+              ))}
+            </>
+          ) : (
+            <Alert severity="info" variant="outlined" sx={{ mt: 2 }}>
+              No Exams Published
+            </Alert>
+          )}
+        </>
       )}
     </Box>
   );
